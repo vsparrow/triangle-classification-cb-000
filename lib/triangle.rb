5543cd19bb376b6293ad0de136e7cbf62d7cb540
vsparrow <khan@vsparrow.com>
2018-03-02 12:47:51 -0500

    practice compelted

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,37 +1,39 @@
 class Triangle
   # write code here
-  attr_accessor :side1,:side2,:side3
+
+  # Write a Triangle class that accepts three arguments on initialization.
+  # Each argument is a length of one of the three sides of the triangle.
   def initialize(side1,side2,side3)
-    @side1=side1
-    @side2=side2
-    @side3=side3
+    @a=side1
+    @b=side2
+    @c=side3
   end
 
-  def kind
-  # The sum of the lengths of any two sides of a triangle always exceeds the length of the third side.
-  # This is a principle known as the triangle inequality.
-  #   Further, each side must be larger than 0.
-    if @side1 <= 0 || @side2 <=0 || @side3<=0
-      raise TriangleError
-    end #if
+  def kind  #returns, as a symbol,  type.  # :equilateral  # :isosceles # :scalene
+      if @a == 0 || @b == 0 || @c == 0
+        raise TriangleError
+      end
+      if (@a + @b <= @c) or (@a + @c <= @b) or (@b + @c <= @a)
+        raise TriangleError
+      end
 
-    if @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 ||  @side2 + @side3 <= @side1
-      raise TriangleError
-    end #if
-    triangle  = nil
-    if @side1 != @side2 && @side1 != @side3 && @side2 != @side3
-      triangle= :scalene
-    elsif @side1 == @side2 && @side2 == @side3
-      triangle = :equilateral
-    # elsif @side1 == @side2 && @side1 != @side3 ||  @side1 == @side3 && @side2 != @side3
-    elsif @side1 == @side3 || @side1 == @side2 || @side2 == @side3
-      triangle= :isosceles
-    end #if
-    triangle
-  end #kind
+      triangle_type = nil
+      triangle_type = :isosceles  if @a == @b || @b == @c || @a == @c
+      triangle_type = :equilateral if @a == @b && @b == @c
+      triangle_type = :scalene if @a != @b && @b != @c && @c != @a
 
-end  #Triangle
+      triangle_type
+  end
+
+
+  # The kind method should raise a custom error, TriangleError if the triangle is invalid.
+
+end
+
 
 class TriangleError < StandardError
-
-end #TriangleError
+  # The sum of the lengths of any two sides of a triangle always exceeds the length of the third side.
+  #  This is a principle known as the triangle inequality.
+  #
+  #  Further, each side must be larger than 0.
+end
